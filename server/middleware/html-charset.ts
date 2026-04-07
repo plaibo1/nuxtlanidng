40be77af8399d85ad6712797c8e8369a202e5618
leopold <leopold@MacBook-Air-leopold.local>
2026-04-07 18:22:42 +0300

    add middleware

---
+++ server/middleware/html-charset.ts
@@ -0,0 +1,14 @@
+import { defineEventHandler } from 'h3'
+
+export default defineEventHandler((event) => {
+  // Устанавливаем правильный Content-Type для HTML-ответов
+  const originalEnd = event.node.res.end.bind(event.node.res)
+
+  event.node.res.end = function (...args: any[]) {
+    const contentType = event.node.res.getHeader('content-type')
+    if (typeof contentType === 'string' && contentType.includes('text/html')) {
+      event.node.res.setHeader('content-type', 'text/html; charset=utf-8')
+    }
+    return originalEnd(...args)
+  }
+})
